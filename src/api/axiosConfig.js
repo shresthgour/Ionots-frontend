7bc0e8e7c3606789a5083bd91145be79ce06da41
Shresth Gour <akshat4575@gmail.com>
2024-12-16 20:52:52 +0530

    `Update API base URL in axiosConfig.js`

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.API;
+const API_BASE_URL = 'https://ionots-backend-production.up.railway.app';
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
